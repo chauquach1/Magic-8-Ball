c86e71be5ff86b8c288460c684ba1d5be49ecf51
chauquach1 <110010009+chauquach1@users.noreply.github.com>
2022-08-02 20:50:47 -0700

    Update main.js

--- main.js
+++ main.js
@@ -22,11 +22,11 @@ if (userName){
 // var eightBall =''
 
 let randomNumber = Math.floor(Math.random() * 8);
-// var eightBall = '';
 var responses = ['It is certain', 'It is decidedly so', 
 'Reply hazy try again','Cannot predict now',
 'Do not count on it','My sources say no',
 'Outlook not so good', 'Signs point to yes'];
+var eightBall = '';
 
 // console.log(responses[1]);
 
